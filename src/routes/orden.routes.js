c562241b5f2ff19290f7e09af57420272c948ff7
MateoSGOT <patinor.mateo@gmail.com>
2026-04-21 10:47:44 -0500

    fix: add /mano-de-obra route alias

--- src/routes/orden.routes.js
+++ src/routes/orden.routes.js
@@ -5,17 +5,18 @@ const { checkPermiso } = require('../middlewares/checkPermiso');
 
 const router = Router();
 
-router.get('/',                       authenticate, checkPermiso('ORDENES.LISTAR'),        ordenController.getAll);
-router.get('/:id',                    authenticate, checkPermiso('ORDENES.CONSULTAR'),     ordenController.getById);
-router.put('/:id',                    authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.update);
-router.patch('/:id/estado',           authenticate, checkPermiso('ORDENES.CAMBIAR_ESTADO'), ordenController.patchEstado);
-router.patch('/:id/flujo',            authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.patchFlujo);
-router.patch('/:id/manodeobra',       authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.patchManoDeObra);
-router.post('/:id/servicios',         authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.addServicio);
-router.post('/:id/servicios/libre',   authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.addServicioLibre);
-router.delete('/:id/servicios/:servicioId', authenticate, checkPermiso('ORDENES.EDITAR'), ordenController.deleteServicio);
-router.post('/:id/repuestos',         authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.addRepuesto);
-router.post('/:id/repuestos/libre',   authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.addRepuestoLibre);
-router.delete('/:id/repuestos/:repuestoId', authenticate, checkPermiso('ORDENES.EDITAR'), ordenController.deleteRepuesto);
+router.get('/',                            authenticate, checkPermiso('ORDENES.LISTAR'),         ordenController.getAll);
+router.get('/:id',                         authenticate, checkPermiso('ORDENES.CONSULTAR'),      ordenController.getById);
+router.put('/:id',                         authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.update);
+router.patch('/:id/estado',                authenticate, checkPermiso('ORDENES.CAMBIAR_ESTADO'), ordenController.patchEstado);
+router.patch('/:id/flujo',                 authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.patchFlujo);
+router.patch('/:id/manodeobra',            authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.patchManoDeObra);
+router.patch('/:id/mano-de-obra',          authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.patchManoDeObra);
+router.post('/:id/servicios',              authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.addServicio);
+router.post('/:id/servicios/libre',        authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.addServicioLibre);
+router.delete('/:id/servicios/:servicioId', authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.deleteServicio);
+router.post('/:id/repuestos',              authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.addRepuesto);
+router.post('/:id/repuestos/libre',        authenticate, checkPermiso('ORDENES.EDITAR'),         ordenController.addRepuestoLibre);
+router.delete('/:id/repuestos/:repuestoId', authenticate, checkPermiso('ORDENES.EDITAR'),        ordenController.deleteRepuesto);
 
 module.exports = router;
