c485cf10602bf28f6658999bbf6b8deb97ad1f74
MateoSGOT <patinor.mateo@gmail.com>
2026-04-21 10:39:32 -0500

    fix: add anular action to compra controller

--- src/controllers/compra.controller.js
+++ src/controllers/compra.controller.js
@@ -3,45 +3,34 @@ const { createSchema } = require('../validators/compra.validator');
 const { BadRequestError } = require('../errors/httpErrors');
 
 const getAll = async (req, res, next) => {
-  try {
-    const compras = await compraService.getAll();
-    res.json({ status: 'ok', data: compras });
-  } catch (err) {
-    next(err);
-  }
+  try { res.json({ status: 'ok', data: await compraService.getAll() }); } catch (err) { next(err); }
 };
 
 const getById = async (req, res, next) => {
-  try {
-    const compra = await compraService.getById(Number(req.params.id));
-    res.json({ status: 'ok', data: compra });
-  } catch (err) {
-    next(err);
-  }
+  try { res.json({ status: 'ok', data: await compraService.getById(Number(req.params.id)) }); } catch (err) { next(err); }
 };
 
 const create = async (req, res, next) => {
   try {
     const { error, value } = createSchema.validate(req.body, { abortEarly: false });
-    if (error) throw new BadRequestError(error.details.map((d) => d.message).join(', '));
-
+    if (error) throw new BadRequestError(error.details.map(d => d.message).join(', '));
     const compra = await compraService.create(value);
     res.status(201).json({ status: 'ok', data: compra });
-  } catch (err) {
-    next(err);
-  }
+  } catch (err) { next(err); }
 };
 
 const patchEstado = async (req, res, next) => {
   try {
     const { Estado } = req.body;
     if (Estado === undefined) throw new BadRequestError('El campo Estado es obligatorio');
-
-    const compra = await compraService.toggleEstado(Number(req.params.id), Estado);
-    res.json({ status: 'ok', data: compra });
-  } catch (err) {
-    next(err);
-  }
+    res.json({ status: 'ok', data: await compraService.toggleEstado(Number(req.params.id), Estado) });
+  } catch (err) { next(err); }
 };
 
-module.exports = { getAll, getById, create, patchEstado };
+const anular = async (req, res, next) => {
+  try {
+    res.json({ status: 'ok', data: await compraService.toggleEstado(Number(req.params.id), false) });
+  } catch (err) { next(err); }
+};
+
+module.exports = { getAll, getById, create, patchEstado, anular };
