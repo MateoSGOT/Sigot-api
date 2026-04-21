c71423ba40aa2c759ad2fb6a007f9623770f31ef
MateoSGOT <patinor.mateo@gmail.com>
2026-04-21 10:39:31 -0500

    fix: add /anular route to compras

--- src/routes/compra.routes.js
+++ src/routes/compra.routes.js
@@ -5,9 +5,10 @@ const { checkPermiso } = require('../middlewares/checkPermiso');
 
 const router = Router();
 
-router.get('/',             authenticate, checkPermiso('COMPRAS.LISTAR'),    compraController.getAll);
-router.get('/:id',          authenticate, checkPermiso('COMPRAS.CONSULTAR'), compraController.getById);
-router.post('/',            authenticate, checkPermiso('COMPRAS.REGISTRAR'), compraController.create);
-router.patch('/:id/estado', authenticate, checkPermiso('COMPRAS.ANULAR'),    compraController.patchEstado);
+router.get('/',              authenticate, checkPermiso('COMPRAS.LISTAR'),    compraController.getAll);
+router.get('/:id',           authenticate, checkPermiso('COMPRAS.CONSULTAR'), compraController.getById);
+router.post('/',             authenticate, checkPermiso('COMPRAS.REGISTRAR'), compraController.create);
+router.patch('/:id/estado',  authenticate, checkPermiso('COMPRAS.ANULAR'),    compraController.patchEstado);
+router.patch('/:id/anular',  authenticate, checkPermiso('COMPRAS.ANULAR'),    compraController.anular);
 
 module.exports = router;
